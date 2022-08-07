293e3bb82f2497e9a9878eb79037fc9a42b2f184
BenchSL <timzupanc@gmail.com>
2022-08-07 16:53:59 +0200

    Adding dictionary for hardware chart monitoring, adding live charts library.

--- GameCheckerWpf/GameCheckerWpf/Models/HardwareMonitor.cs
+++ GameCheckerWpf/GameCheckerWpf/Models/HardwareMonitor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -14,6 +15,9 @@ namespace GameCheckerWpf.Models
         private string countMemory;
         int countCpuInt;
         int countMemoryInt;
+        double realTimeCountMemoryUsage;
+        double realTimeCountCPUUsage;
+        Dictionary<int, double> performanceTracker;
 
         public string CountCpu
         {
@@ -66,6 +70,33 @@ namespace GameCheckerWpf.Models
                 OnPropertyChanged(nameof(CountMemoryInt));
             }
         }
+        public double RealTimeCountMemoryUsage
+        {
+            get { return realTimeCountMemoryUsage; }
+            set
+            {
+                realTimeCountMemoryUsage = value;
+                OnPropertyChanged(nameof(RealTimeCountMemoryUsage));
+            }
+        }
+        public double RealTimeCountCpuUsage
+        {
+            get { return realTimeCountCPUUsage; }
+            set
+            {
+                realTimeCountCPUUsage = value;
+                OnPropertyChanged(nameof(RealTimeCountCpuUsage));
+            }
+        }
+        public Dictionary<int, double> PerformanceTracker
+        {
+            get { return performanceTracker; }
+            set
+            {
+                performanceTracker = value;
+                OnPropertyChanged(nameof(PerformanceTracker));
+            }
+        }
 
         private System.Timers.Timer _timer;
         PerformanceCounter myAppCPU = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName, true);
@@ -73,6 +104,7 @@ namespace GameCheckerWpf.Models
 
         public HardwareMonitor()
         {
+            PerformanceTracker = new Dictionary<int, double>();
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += DispatcherTimer_Tick;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
@@ -87,6 +119,33 @@ namespace GameCheckerWpf.Models
 
             CountMemoryInt = (int)MyMem.NextValue();
             CountMemory = $"{CountMemoryInt.ToString()}%";
+            double infomem = getRandomAccessMemoryInfo();
+            double test = infomem / 100;
+            RealTimeCountMemoryUsage = test * infomem;
+
+            PerformanceTracker.Add(CountMemoryInt, RealTimeCountMemoryUsage*3);
+        }
+
+        private int getRandomAccessMemoryInfo()
+        {
+            ManagementObjectSearcher ramSearcher = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+            string myRAMsize = "";
+
+            foreach (ManagementObject ramObj in ramSearcher.Get())
+            {
+                myRAMsize = ramObj["TotalVirtualMemorySize"].ToString();
+            }
+
+            int ramInt = int.Parse(myRAMsize);
+            int totalInt = (ramInt / 1000000);
+
+            return totalInt;
+        }
+
+        private Dictionary<int, double> returnDictionaryTest()
+        {
+            return PerformanceTracker;
         }
     }
 }
