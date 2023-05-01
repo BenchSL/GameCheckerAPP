815b0698373dfb16a677dc5e909d41ec0999dd8a
BenchSL <timzupanc@gmail.com>
2023-05-02 01:41:44 +0200

    added migration

---
+++ GameCheckerAPI/GameCheckerAPI/Migrations/20230501234119_ddddd.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameCheckerAPI.Migrations
+{
+    public partial class ddddd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "computerHardware",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CPU = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RAM = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OS = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GraphicsCard = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DiskSpaceRequired = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_computerHardware", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "computerHardware");
+        }
+    }
+}
